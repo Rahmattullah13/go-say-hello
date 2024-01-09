f5ead0472463383563875a8d57c6090ad98b3257
Rahmattullah <rfkrhmttllh@gmail.com>
2024-01-09 09:59:49 +0700

    update say hello with parameters

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,7 @@
 package gosayhello
 
-func SayHello(name string) string {
-	return "Hello " + name
+import "fmt"
+
+func SayHello(name string, address string) string {
+	return fmt.Sprintf("Hello, nama saya %s dan saya berasal dari %s.", name, address)
 }
